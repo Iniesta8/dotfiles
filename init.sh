fc1fa215ce0bf30d336a684514a69cb4d0201fb3
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-05-11 07:27:38 +0200

    Unlock ssh keys on login

--- init.sh
+++ init.sh
@@ -3,7 +3,4 @@
 #SESSION_NAME=${PWD##*/} # Current directory name will be used as session name
 SESSION_NAME=dev
 
-#eval `ssh-agent -s` >/dev/null
-ssh-add
-
 TERM=screen-256color tmux new -t $SESSION_NAME
