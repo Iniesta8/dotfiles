1a68d4e68e436d9a25a1a6682041cef755632eeb
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-12-28 12:14:25 +0100

    Add lua based modern neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,46 @@
+require('basics')
+require('bindings')
+require('colors')
+require('telescope-config')
+require('coc-config')
+require('functions')
+require('lualine').setup()
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true
+  }
+}
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use 'folke/tokyonight.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'tpope/vim-commentary'
+  use 'ThePrimeagen/git-worktree.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    "blackCauldron7/surround.nvim",
+    config = function()
+      require"surround".setup {mappings_style = "surround"}
+    end
+  }
+  use 'preservim/nerdcommenter'
+  use 'machakann/vim-highlightedyank'
+  use 'rhysd/vim-clang-format'
+end)
---
+++ .config/nvim/lua/bindings.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_set_keymap('n', '<leader>w', ':w!<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>Q', ':q<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>q', ':bd<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>,', '<C-^>', { noremap = true})
+-- clear search
+vim.api.nvim_set_keymap('n', '<leader>qs', '<Esc>:noh<CR>', { noremap = true})
+-- find merge conflict markers
+vim.api.nvim_set_keymap('n', '<leader>fc', '/\\v^[<\\|=>]{7}( .*\\|$)<CR>', { noremap = true})
+
