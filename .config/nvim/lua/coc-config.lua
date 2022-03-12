c4e380d2329ef5b12f5fcf5b915c98beefdc8511
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2022-03-12 12:45:02 +0100

    Update configs

--- .config/nvim/lua/coc-config.lua
+++ .config/nvim/lua/coc-config.lua
@@ -1,5 +1,4 @@
 vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
---vim.api.nvim_set_keymap("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
 vim.api.nvim_set_keymap("n", "gD", "<Plug>(coc-declaration)", {silent = true})
 vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", {silent = true})
@@ -11,6 +10,7 @@ vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr
 vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
 vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
+
 vim.o.hidden = true
 vim.o.backup = false
 vim.o.writebackup = false
